2b01c80d13b67f0fa26cc4327ebff5024903495d
Stefan Ronacher <stefan.ronacher.78@gmail.com>
2020-05-14 12:16:27 -0300

    Menu funcionando

---
+++ resources/views/includes/old_top_bar.blade.php
@@ -0,0 +1,84 @@
+<div class="dropdown">
+
+
+<!--
+            <a class="navbar-brand" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <div>
+                Presupuestos
+              </div>
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+
+              <a class="dropdown-item1" href="/budget/make">
+                Hacer&nbsp;
+              </a>
+              <a class="dropdown-item1" href="budget/view">
+                Mostrar&nbsp;
+              </a>
+            </div>
+-->
+
+<a class="navbar-brand" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+  <div>
+  Configuración2
+  </div>
+</a>
+<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+<li class="dropdown-submenu">
+  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+
+        Precios papeles2
+
+  </a>
+  <ul class="dropdown-submenu">
+    <li><a href="/configuration/load_paper_prices">Cargar2</a></li>
+    <li><a href="/configuration/show_paper_prices">Mostrar2</a></li>
+  </ul>
+</li>
+<li class="dropdown-submenu">
+  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+
+        Precio dólar2
+
+  </a>
+  <ul class="dropdown-submenu">
+    <li><a href="/configuration/set_dollar_price">Cargar3</a></li>
+    <li><a href="/configuration/show_dollar_prices">Mostrar3</a></li>
+  </ul>
+</li>
+</div>
+
+
+
+
+          <a class="navbar-brand" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <div>
+            Configuración
+            </div>
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <li class="dropdown-submenu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+
+                  Precios papeles
+
+            </a>
+            <ul class="dropdown-submenu">
+              <li><a href="/configuration/load_paper_prices">Cargar</a></li>
+              <li><a href="/configuration/show_paper_prices">Mostrar</a></li>
+            </ul>
+          </li>
+          <li class="dropdown-submenu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+
+                  Precio dólar
+
+            </a>
+            <ul class="dropdown-submenu">
+              <li><a href="/configuration/set_dollar_price">Cargar</a></li>
+              <li><a href="/configuration/show_dollar_prices">Mostrar</a></li>
+            </ul>
+          </li>
+          </div>
+</div>
+<div style="min-height:785px">
--- resources/views/includes/top_bar.blade.php
+++ resources/views/includes/top_bar.blade.php
@@ -1,53 +1,53 @@
-<div class="dropdown">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="/">Home</a></li>
+      &nbsp;&nbsp;
+      <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true"> <span class="nav-label">Configuración</span> <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+              <li class="dropdown-submenu">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> <span class="nav-label">Precios papeles</span><span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                      <li><a href="/configuration/load_paper_prices">Cargar</a></li>
+                      <li><a href="/configuration/show_paper_prices">Mostrar</a></li>
+                  </ul>
+              </li>
+              <li class="dropdown-submenu">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> <span class="nav-label">Precio dólar</span><span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                      <li><a href="/configuration/set_dollar_price">Cargar</a></li>
+                      <li><a href="/configuration/show_dollar_prices">Mostrar</a></li>
+                  </ul>
+              </li>
+          </ul>
+      </li>
 
-<!--
-            <a class="navbar-brand" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <div>
-                Presupuestos
-              </div>
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+      &nbsp;&nbsp;
+      <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true"> <span class="nav-label">Presupuestos</span> <span class="caret"></span></a>
+          <ul class="dropdown-menu">
 
-              <a class="dropdown-item1" href="/budget/make">
-                Hacer&nbsp;
-              </a>
-              <a class="dropdown-item1" href="budget/view">
-                Mostrar&nbsp;
-              </a>
-            </div>
--->
+              <li class="dropdown-submenu">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> <span class="nav-label">Service C</span><span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                      <li><a href="#">Service C1</a></li>
+                      <li><a href="#">Service C2</a></li>
+                      <li><a href="#">Service C3</a></li>
+                      <li><a href="#">Service C4</a></li>
+                      <li><a href="#">Service C5</a></li>
+                  </ul>
+              </li>
+          </ul>
+      </li>
 
+  </ul>
 
+  </div>
+</nav>
 
-          <a class="navbar-brand" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <div>
-            Configuración
-            </div>
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <li class="dropdown-submenu">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-
-                  Precios papeles
-
-            </a>
-            <ul class="dropdown-menu">
-              <li><a href="/configuration/load_paper_prices">Cargar</a></li>
-              <li><a href="/configuration/show_paper_prices">Mostrar</a></li>
-            </ul>
-          </li>
-          <li class="dropdown-submenu">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-
-                  Precio dólar
-
-            </a>
-            <ul class="dropdown-menu">
-              <li><a href="/configuration/set_dollar_price">Cargar</a></li>
-              <li><a href="/configuration/show_dollar_prices">Mostrar</a></li>
-            </ul>
-          </li>
-          </div>
-</div>
 <div style="min-height:785px">
